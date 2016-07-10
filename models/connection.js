ffc8a91a0380fbcccfed60515621df8f7627e2b0
Cristian <cm961224@gmail.com>
2016-07-10 20:42:30 +0000

    nueva conexion

--- models/connection.js
+++ models/connection.js
@@ -1,7 +1,7 @@
 var mysql = require('mysql');
 module.exports.Connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : '',
-  database : 'warframe'
+  host     : 'mysql.2freehosting.com',
+  user     : 'u373512054_camz',
+  password : 'tupapitomk24',
+  database : 'u373512054_wfpry'
 });
